e7d0c91f6dbe263f4d6390f05405c13f90e809d1
Avadhut Lohar <loharavdhoot@gmail.com>
2024-01-08 20:53:45 +0530

    day8

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,36 +1,54 @@
 import React, { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
-import { redirect } from "react-router-dom";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState("");
+  const [resInfo, setResInfo] = useState(null);
+
   useEffect(() => {
     fetchMenu();
   }, []);
 
   const fetchMenu = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.4901977&lng=73.8397009&restaurantId=23749&catalog_qa=undefined&submitAction=ENTER"
-    );
-    const json = await data.json();
-    console.log(json);
-    setResInfo(json);
-  };
-  
+    try {
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.4901977&lng=73.8397009&restaurantId=23749"
+      );
+      const json = await data.json();
+      console.log(json);
 
-  return !resInfo ? (
-    <Shimmer />
-  ) : (
+      if (json?.data) {
+        setResInfo(json.data);
+      } else {
+        // Handle the case when data is not in the expected format
+        console.error("Invalid data format:", json);
+      }
+    } catch (error) {
+      // Handle fetch errors
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  // Check if resInfo and nested properties are available
+  const itemCard =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card.card
+      .itemCards;
+
+  if (!resInfo || !itemCard) {
+    // Handle the case when resInfo or itemCard is undefined or null
+    return <Shimmer />; // or render a loading state or redirect to an error page
+  }
+  const resName = resInfo?.cards[0]?.card?.card?.info?.name;
+  const cuisines = resInfo?.cards[0]?.card?.card?.info?.cuisines;
+  const cost = resInfo?.cards[0]?.card?.card?.info?.costForTwo;
+  return (
     <div className="menu">
-      <h1>{resInfo?.data?.cards[0]?.card?.card?.info.name}</h1>
-      <h3>{resInfo?.data?.cards[0]?.card?.card?.info.cuisines.join()}</h3>
-      <h3>{resInfo?.data?.cards[0]?.card?.card?.info.costForTwoMessage}</h3>
+      <h1>{resName}</h1>
+      <h3>{cuisines}</h3>
+      <h3>{"Cost for two : "}{cost/100}</h3>
       <ul>
-        <li>{resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card.card.itemCards[0].card.info.name}</li>
-        <li>{resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card.card.itemCards[1].card.info.name}</li>
-        <li>{resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card.card.itemCards[2].card.info.name}</li>
-        <li>{resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card.card.itemCards[3].card.info.name}</li>
-        
+        {itemCard.map((item, index) => (
+          <li key={index}>{item?.card?.info?.name}- {item?.card?.info?.price /100}</li>
+        ))}
       </ul>
     </div>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,5 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const API_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.4901977&lng=73.8397009&restaurantId=23749&catalog_qa=undefined&submitAction=ENTER";
+
